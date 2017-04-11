6fab689af46fa655f66ddee56cc4771e7d1a9dbc
DakMcMak <esbolov.galymzhan@mail.ru>
2017-04-11 22:39:57 +0600

    Initial commit

---
+++ src/main/java/Friend.java
@@ -0,0 +1,25 @@
+
+public class Friend {
+    private String name;
+    private int age;
+    private char sex;
+
+    public void initialize(String name) {
+        this.name = name;
+    }
+
+    public void initialize(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void initialize(String name, int age, char folder) {
+        this.name = name;
+        this.age = age;
+        this.sex = folder;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
